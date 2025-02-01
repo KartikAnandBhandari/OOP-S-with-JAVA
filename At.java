166835ce616f6e43ac6da3ed1cae5a5595af53d2
KartikAnandBhandari <kartikbhnndari@gmail.com>
2025-02-01 19:47:20 +0530

    Initial commit

---
+++ At.java
@@ -0,0 +1,14 @@
+public class At {
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Usage: java TriangleArea <base> <height>");
+            return;
+        }
+        
+        double base = Double.parseDouble(args[0]);
+        double height = Double.parseDouble(args[1]);
+
+        double area = 0.5 * base * height;
+        System.out.println("Area of the triangle: " + area);
+    }
+}
---
+++ SI.java
@@ -0,0 +1,15 @@
+public class SI{
+    public static void main(String[] args) {
+        if (args.length < 3) {
+            System.out.println("Usage: java SimpleInterest <principal> <rate> <time>");
+            return;
+        }
+        
+        double principal = Double.parseDouble(args[0]);
+        double rate = Double.parseDouble(args[1]);
+        double time = Double.parseDouble(args[2]);
+
+        double interest = (principal * rate * time) / 100;
+        System.out.println("Simple Interest: " + interest);
+    }
+}
